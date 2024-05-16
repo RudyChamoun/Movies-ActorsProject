35399713846275be2ea0e9c7a6b25b26353e628b
Rudy Chamoun <rudychamoun1729@gmail.com>
2024-05-17 01:04:14 +0300

    Third Functionality Succesfull

--- src/app/actor-search/actor-search.component.ts
+++ src/app/actor-search/actor-search.component.ts
@@ -10,10 +10,13 @@ import { TmdbService } from '../tmdb.service';
 export class ActorSearchComponent implements OnInit {
   query: string = '';
   actors: any[] = [];
+  favoriteActors: any[] = [];
 
   constructor(private tmdbService: TmdbService, private router: Router) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.loadFavorites();
+  }
 
   searchActors(): void {
     if (this.query.trim()) {
@@ -26,7 +29,29 @@ export class ActorSearchComponent implements OnInit {
   getPopularMovies(actor: any): string {
     return actor.known_for.map((movie: any) => movie.title).join(', ');
   }
+  toggleFavorite(actor: any): void {
+    if (this.isFavorite(actor)) {
+      this.favoriteActors = this.favoriteActors.filter(fav => fav.id !== actor.id);
+    } else {
+      this.favoriteActors.push(actor);
+    }
+    this.saveFavorites();
+  }
 
+  isFavorite(actor: any): boolean {
+    return this.favoriteActors.some(fav => fav.id === actor.id);
+  }
+
+  saveFavorites(): void {
+    localStorage.setItem('favoriteActors', JSON.stringify(this.favoriteActors));
+  }
+
+  loadFavorites(): void {
+    const storedFavorites = localStorage.getItem('favoriteActors');
+    if (storedFavorites) {
+      this.favoriteActors = JSON.parse(storedFavorites);
+    }
+  }
   viewMovies(actorId: number): void {
     this.router.navigate(['/movies', actorId]);
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common';  // Import CommonModule
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ActorSearchComponent } from './actor-search/actor-search.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { FavoriteActorsComponent } from './favorite-actors/favorite-actors.component';  // Ensure this is imported
 import { TmdbService } from './tmdb.service';
 import { routes } from './app.routes';
 
@@ -15,11 +16,12 @@ import { routes } from './app.routes';
   declarations: [
     AppComponent,
     ActorSearchComponent,
-    MovieListComponent
+    MovieListComponent,
+    FavoriteActorsComponent
   ],
   imports: [
     BrowserModule,
-    CommonModule,
+    CommonModule,  // Ensure CommonModule is imported here
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot(routes)
